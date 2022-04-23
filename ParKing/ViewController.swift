26b74664a1a06c27f50d0bbd28ac34628a528798
Mark Lai <taipomark@gmail.com>
2022-04-23 12:39:26 +0800

    Initial Commit

---
+++ ParKing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParKing
+//
+//  Created by Mark Lai on 23/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
